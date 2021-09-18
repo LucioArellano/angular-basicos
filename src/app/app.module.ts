488fbce6871a3b0e96a7d8af07002a7588fca5cc
LucioArellano <docentelucio@gmail.com>
2021-09-17 22:51:29 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ContadorModule } from './contador/contador.module';
+
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorComponent } from './contador/contador/contador.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
